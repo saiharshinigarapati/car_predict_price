0747b6f795fe904a2834da168b436775716319c5
saiharshinigarapati <48383338+saiharshinigarapati@users.noreply.github.com>
2020-08-08 10:43:11 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask,render_template,request
+import jsonify
+import requests
+import pickle
+import numpy as np
+import sklearn
+from sklearn.preprocessing import StandardScaler
+app=Flask(__name__)
+model=pickle.load(open('rf_regressor_model1.pkl','rb'))
+@app.route('/',methods=['GET'])
+def Home():
+    return render_template('index.html')
+standard_to=StandardScaler()
+@app.route('/predict',methods=['POST'])
+def predict():
+    Fuel_Type_Diesel=0
+    if request.method=='POST':
+        Year=int(request.form['Year'])
+        Present_Price=float(request.form['Present_Price'])
+        kms_driven=int(request.form['Kms_Drive'])    
+        Kms_Driven2=np.log(kms_driven)
+        Owner=int(request.form['Owner'])
+        Fuel_Type_Petrol=request.form['Fuel_Type_Petrol']
+        if Fuel_Type_Petrol=='Petrol':
+            Fuel_Type_Petrol=1
+            Fuel_Type_Diesel=0
+        else:
+            Fuel_Type_Diesel=1
+            Fuel_Type_Petrol=0
+        Year=2020-Year
+        Seller_Type_Individual=request.form['Seller_Type_Individual'] 
+        if(Seller_Type_Individual=='Individual'):
+            Seller_Type_Individual=1
+        else:
+            Seller_Type_Individual=0
+        Transmission_manual=request.form['Transmission_Manual']
+        if Transmission_manual=='Mannual':
+            Transmission_manual=1
+        else:
+            Transmission_manual=0
+        prediction=model.predict([[Present_Price,Kms_Driven2,Owner,Year,Fuel_Type_Diesel,Fuel_Type_Petrol,Seller_Type_Individual,Transmission_manual]])
+        output=round(prediction[0],2) 
+        if output<0:
+            return render_template('index.html',prediction_texts="Sorry you cannot sell this car")
+        else:
+            return render_template('index.html',prediction_text="you can sell the car at {}".format(output))
+    else:
+        return render_template('index.html')
+
+if __name__=="__main__":
+    app.run(debug=True)        
+                                        
